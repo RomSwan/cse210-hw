3cd4c81603cfcf58f89b3de34f967ef273334cd5
RomSwan <legion.of.swans@gmail.com>
2025-05-07 17:14:28 +0200

    Added Grades

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter a grade percentage: ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+
+        string grade = "";
+
+        if (number >= 90)
+        {
+            grade = "A";
+        }
+        else if (number >= 80)
+        {
+            grade = "B";
+        }
+        else if (number >= 70)
+        {
+            grade = "C";
+        }
+        else if (number >= 60)
+        {
+            grade = "D";
+        }
+        else if (number < 60)
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Here is your grade: {grade}");
     }
 }
