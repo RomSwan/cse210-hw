2f428ec627bada7cfc2991bb5ce4258123fe02a4
RomSwan <legion.of.swans@gmail.com>
2025-05-08 14:39:56 +0200

    Finished the stretch challenges

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,35 +4,59 @@ using System.Diagnostics.Tracing;
 class Program
 {
     static void Main(string[] args)
-    {   // Generate random number
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 101);
+    {   
+        while (true)
+        {
+            // Generate random number
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
 
-        // Sets the guess counter
-        int guesses = 0;
+            // Sets the guess counter
+            int guesses = 0;
 
-        while (true) // Keeps the loop infinite
-        {   // User input
-            Console.Write("Enter your guess: ");
-            string input2 = Console.ReadLine();
-            int guess = int.Parse(input2);
+            while (true) // Keeps the loop infinite
+            {   // User input
+                Console.Write("Enter your guess: ");
+                string input2 = Console.ReadLine();
+                int guess = int.Parse(input2);
 
-            // Adds to guess counter
-            guesses += 1;
+                // Adds to guess counter
+                guesses += 1;
 
-            if (guess > number)
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower.");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher.");
+                }
+                else
+                {   // user successfully guesses the correct number
+                    Console.WriteLine($"Correct! It took you {guesses} guesses.");
+                    break; // Breaks the loop
+                }
+            }    
+
+            // Ask to go again
+            Console.Write("Go again? yes/no: ");
+            string yes_no = Console.ReadLine();
+            if (yes_no == "yes")
             {
-                Console.WriteLine("Lower.");
+                guesses = 0; // Resets guess counter 
+                continue; // Restarts loop
             }
-            else if (guess < number)
+            else if (yes_no == "no")
             {
-                Console.WriteLine("Higher.");
-            }
-            else
-            {   // They successfully guess the correct number
-                Console.WriteLine($"Correct! It took you {guesses} guesses.");
+                Console.WriteLine("Okay. See you later!");
                 break; // Breaks loop, which ends the program.
             }
+            else // If input is neither yes nor no, just restart the loop for fun.
+            {
+                Console.WriteLine("Hm. I didn't quite catch that. I'll assume you wanna go again.");
+                guesses = 0; // Resets guess counter 
+                continue; // Restarts loop
+            }       
         }
     }
 }
