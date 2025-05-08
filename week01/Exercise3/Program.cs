038184682e1401188f9f7818e874a0d6e23cec49
RomSwan <legion.of.swans@gmail.com>
2025-05-08 14:03:22 +0200

    Changed the random number to be inbetween 1 and 100

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -6,7 +6,7 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 11);
+        int number = randomGenerator.Next(1, 101);
 
         while (true)
         {
