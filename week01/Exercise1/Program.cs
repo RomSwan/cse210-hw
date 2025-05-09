ee011eba386060a553122be858952b9ba5b21b9c
RomSwan <legion.of.swans@gmail.com>
2025-05-09 14:36:00 +0200

    Cleaned final code

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,19 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-<<<<<<< HEAD
+        // Asks for first name
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
 
+        // Asks for last name
         Console.Write("What is your last name? ");
-=======
-        Console.Write("Enter your first name: ");
-        string first = Console.ReadLine();
-
-        Console.Write("Enter your last name: ");
->>>>>>> 56ac07b48a2bc3e19d8c71d7688eb576f305488a
         string last = Console.ReadLine();
 
+        // Display result
         Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
