92b881faa5621ff993e13fe1fb9169bfbcdae02c
RomSwan <legion.of.swans@gmail.com>
2025-05-09 14:02:13 +0200

    Completed Stretch challenges

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,10 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {   // The list of numbers & the declared variables
-        List<string> numbers = new List<string>();
+        List<int> numbers = new List<int>();
         int sum = 0;
         int average = 0;
         int maximum = 0;
+        int smallest_positive = int.MaxValue;
+
 
         // Intro
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
@@ -15,9 +17,11 @@ class Program
         while (true) // Keeps loop going until broken
         {   // User input
             Console.Write("Enter a number: ");
-            string number = Console.ReadLine();
+            string input = Console.ReadLine();
+            // Convert from string to int:
+            int number = int.Parse(input);
 
-            if (number == "0") // Breaks loop if 0 is entered
+            if (number == 0) // Breaks loop if 0 is entered
             {
                 break;
             }
@@ -27,27 +31,34 @@ class Program
             }
         }
 
-        // SUM of numbers
-        foreach (string number in numbers)
-        {
-            int num = int.Parse(number);
-            sum = sum + num;
-        }
-        // AVERAGE of numbers
-        average = sum / numbers.Count();
-        // MAXIMUM of numbers
-        foreach (string number in numbers)
-        {
-            int num = int.Parse(number);
-            if (num > maximum) // If a number is greater than the maximum, make it the new maximum
+        // Loop for calculations
+        foreach (int number in numbers)
+        {   // SUM
+            sum = sum + number;
+            // MAXIMUM
+            if (number > maximum)
             {
-                maximum = num;
+                maximum = number;
+            }
+            // SMALLEST POSITIVE
+            if (number > 0 && number < smallest_positive)
+            {
+                smallest_positive = number;
             }
         }
+        // AVERAGE
+        average = sum / numbers.Count();
 
-        // Print the sum, average and maximum:
+        // Print the sum, average, maximum, smallest positive, and sorted list:
         Console.WriteLine($"The sum is: {sum}");
         Console.WriteLine($"The average is: {average}");
         Console.WriteLine($"The maximum is: {maximum}");
+        Console.WriteLine($"The smallest positive number is: {smallest_positive}");
+        Console.WriteLine($"The sorted list is:");
+        numbers.Sort(); // Sort list
+        foreach (int number in numbers) // Print the sorted list
+        {
+            Console.WriteLine(number);
+        }
     }
 }  
