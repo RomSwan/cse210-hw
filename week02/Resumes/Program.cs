d3714dc90793e3e4120f0fe834ffa53c2849bf9d
RomSwan <legion.of.swans@gmail.com>
2025-05-13 14:19:05 +0200

    Finished W02 Learning Activity: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Runtime.ConstrainedExecution;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "2005";
+        job1._endYear = "2015";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Salesman";
+        job2._company = "Apple";
+        job2._startYear = "2022";
+        job2._endYear = "2025";
+
+        Resume resume = new Resume();
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume._name = "John Smith";
+
+        resume.DisplayJobDetails();
     }
 }
