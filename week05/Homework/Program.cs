f9fd24faae765212b1772bcf57552274e2e7a02e
RomSwan <legion.of.swans@gmail.com>
2025-06-04 15:57:29 +0200

    Finished Learning Activity 05

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment student1 = new Assignment("George Harrison", "Geometry");
+        Console.WriteLine(student1.GetSummary());
+
+        MathAssignment student2 = new MathAssignment("Michael Jackson", "Matricies", "4.5", "1-12");
+        Console.WriteLine(student2.GetSummary());
+        Console.WriteLine(student2.GetHomeworkList());
+
+        WritingAssignment student3 = new WritingAssignment("Diana Washington", "Greek Mythology", "The Legend of the Amazonians");
+        Console.WriteLine(student3.GetSummary());
+        Console.WriteLine(student3.GetWritingInformation());
     }
 }
